dd3b79880ac00aacf1f406a1d525e8a1e10e60fc
Stewart Wingfield <sww915@gmail.com>
2023-06-25 17:27:39 -0500

    all 3 passing

--- main.js
+++ main.js
@@ -14,10 +14,22 @@ const rl = readline.createInterface({
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+  hand1 = hand1.toLowerCase()
+  hand1 = hand1.trim()
 
-}
+  hand2 = hand2.toLowerCase()
+  hand2 = hand2.trim()
+  
+  if (hand1 === hand2){
+    return "It's a tie!"
+  } else if ((hand1 === 'rock') && (hand2 === 'scissors')) {
+    return "Hand one wins!"
+  } else if ((hand1 === 'rock') && (hand2 === 'paper')) {
+    return "Hand two wins!"
+  } else ((hand1 === 'paper') && (hand2 === 'scissors')); {
+    return "Hand two wins!"
+  } 
+} 
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
